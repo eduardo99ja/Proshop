bcd470a351530e12e60d2ab6ef20a1e8ebf52606
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-10-26 21:10:53 -0600

    generate a JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,27 @@
 import asyncHandler from 'express-async-handler'
+import generatedToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // *desc    Auth user & get token
 // *route   POST /api/users/login
 // *access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const{email,password} =req.body
-  
-  const user = await User.findOne({email:email})
+  const { email, password } = req.body
 
-  if(user && (await user.matchPassword(password))){
+  const user = await User.findOne({ email: email })
+
+  if (user && (await user.matchPassword(password))) {
     res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: null
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generatedToken(user._id),
     })
-  }else{
-      res.status(401)
-      throw new Error('Invalid email or password')
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-  
 })
 
-
-export {
-    authUser
-}
+export { authUser }
