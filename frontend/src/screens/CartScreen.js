b00fe9fa3c4dd55be8952f504c1b86b1caf651a4
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-10-26 19:12:47 -0600

    remove items from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 //TODO: fix bug that not allow to modify qty the last product
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
@@ -22,11 +22,11 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty])
 
   const removeFromCartHandler = (id) => {
-    console.log('remove')
+    dispatch(removeFromCart(id))
   }
 
-  const checkoutHandler =()=>{
-      history.push('/login?redirect=shipping')
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
   }
   return (
     <Row>
@@ -81,17 +81,30 @@ const CartScreen = ({ match, location, history }) => {
         )}
       </Col>
       <Col md={4}>
-          <Card>
-              <ListGroup>
-                  <ListGroup.Item>
-                      <h2>Subtotal({cartItems.reduce((acc,item)=> acc + item.qty,0 )}) items</h2>
-                      ${cartItems.reduce((acc,item)=> acc + item.qty * item.price,0).toFixed(2)}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                      <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>Proceed to checkout</Button>
-                  </ListGroup.Item>
-              </ListGroup>
-          </Card>
+        <Card>
+          <ListGroup>
+            <ListGroup.Item>
+              <h2>
+                Subtotal({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed to checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   )
