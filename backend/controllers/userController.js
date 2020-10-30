6ea9667f94e575198d338b80a655d5216c308557
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-10-30 12:46:29 -0600

    get user by ID and update user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if (user) {
-       // status 201 .- something was created
+    // status 201 .- something was created
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -52,8 +52,8 @@ const registerUser = asyncHandler(async (req, res) => {
       token: generatedToken(user._id),
     })
   } else {
-      res.status(400)
-      throw new Error('Invalid user data')
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
 // *desc    Get user profile
@@ -103,7 +103,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-
 // @desc    Get all users
 // @route   GET /api/users
 // @access  Private/Admin
@@ -126,6 +125,44 @@ const deleteUser = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin || user.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
 export {
   authUser,
@@ -134,4 +171,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
