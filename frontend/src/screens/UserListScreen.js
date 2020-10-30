5fe2942d6502d3154100c1cbe2cb173fd9e3347e
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-10-30 13:10:32 -0600

    User edit screen and get user details

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -15,7 +15,7 @@ const UserListScreen = ({ history }) => {
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
-  const userDelete = useSelector((state) => state.userDelete)
+  const userDelete = useSelector(state => state.userDelete)
   const { success: successDelete } = userDelete
 
   useEffect(() => {
@@ -24,7 +24,7 @@ const UserListScreen = ({ history }) => {
     } else {
       history.push('/login')
     }
-  }, [dispatch, history, successDelete])
+  }, [dispatch, history, successDelete, userInfo])
 
   const deleteHandler = id => {
     if (window.confirm('Are you sure')) {
@@ -66,7 +66,7 @@ const UserListScreen = ({ history }) => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
                     <Button variant='light' className='btn-sm'>
                       <i className='fas fa-edit'></i>
                     </Button>
