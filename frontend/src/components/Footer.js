667e13275b91bcf3b378f25f4a250986ba0d0bb6
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-01 12:24:06 -0600

    footer - personal data

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,19 +1,47 @@
 import React from 'react'
-import { Container,Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">
-                        Copyright &copy;
-
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+  const labelStyle = {
+    color: 'white',
+  }
+  return (
+    <footer className='bg-primary'>
+      <Container style={labelStyle} className='pt-3 pb-3'>
+        <Row className='text-center '>
+          <Col>
+            <label htmlFor=''>
+              Diseñado por: <strong>Eduardo Apodaca</strong> |{' '}
+              <a
+                style={labelStyle}
+                href='https://eduardo99ja.github.io/portfolio/'
+                target='_blank'
+              >
+                Visita mi sitio web
+              </a>
+              <br />
+              ¿Necesitas una pagina web?
+              <br />
+              Contáctame mandando un correo a{' '}
+              <strong>
+                {' '}
+                <a
+                  style={labelStyle}
+                  href='mailto:eduardo1ja99@gmail.com'
+                  target='_blank'
+                >
+                  eduardo1ja99@gmail.com
+                </a>{' '}
+              </strong>
+            </label>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='text-center'>Copyright &copy; | Todos los derechos reservados</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
